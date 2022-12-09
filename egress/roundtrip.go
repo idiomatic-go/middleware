6ff9d1d1dd6f0773077575663aef3f0fe7ef3b75
idiomatic-go <markbwilson@outlook.com>
2022-12-08 22:12:37 -0600

    Refactoring

--- egress/roundtrip.go
+++ egress/roundtrip.go
@@ -13,33 +13,27 @@ type wrapper struct {
 }
 
 func (w wrapper) RoundTrip(req *http.Request) (*http.Response, error) {
-	var start time.Time
+	var start = time.Now()
 	var flags string
 	var resp *http.Response
 	var err error
 
 	route := Lookup(req)
-	allow := route.Allow()
-	if !allow {
+	if !route.Allow() {
 		flags = accesslog.RateLimitFlag
-	}
-	if route.IsLogging() {
-		start = time.Now()
-	}
-	if allow {
+		resp = &http.Response{Request: req, StatusCode: 503}
+	} else {
 		if route.IsTimeout() {
 			ctx, cancel := context.WithTimeout(req.Context(), route.Duration())
 			defer cancel()
 			req = req.Clone(ctx)
 		}
-		resp, err = w.RoundTrip(req) //http.DefaultTransport.RoundTrip(req)
-	} else {
-		resp = &http.Response{Request: req, StatusCode: 503}
-	}
-	// TODO : check on timeout
-	if err != nil && errors.As(err, context.DeadlineExceeded) {
-		flags = accesslog.UpstreamTmeoutFlag
-		resp.StatusCode = 504
+		resp, err = w.RoundTrip(req)
+		// TODO : check on timeout
+		if err != nil && errors.As(err, context.DeadlineExceeded) {
+			flags = accesslog.UpstreamTmeoutFlag
+			resp.StatusCode = 504
+		}
 	}
 	if route.IsLogging() {
 		accesslog.WriteEgress(start, time.Since(start), route, req, resp, flags)
--- route/route.go
+++ route/route.go
@@ -20,6 +20,14 @@ func (r *Route) IsTimeout() bool {
 	return r != nil && r.Timeout != 0
 }
 
+func (r *Route) IsLogging() bool {
+	return r != nil && r.WriteAccessLog
+}
+
+func (r *Route) IsRateLimiter() bool {
+	return r != nil && r.rateLimiter != nil
+}
+
 func (r *Route) Duration() time.Duration {
 	if r == nil {
 		return 0
@@ -27,17 +35,9 @@ func (r *Route) Duration() time.Duration {
 	return time.Duration(r.Timeout) * time.Millisecond
 }
 
-func (r *Route) IsLogging() bool {
-	return r != nil && r.WriteAccessLog
-}
-
 func (r *Route) Allow() bool {
 	if r == nil || r.rateLimiter == nil {
 		return true
 	}
 	return r.rateLimiter.Allow()
-}
-
-func (r *Route) IsRateLimiter() bool {
-	return r != nil && r.rateLimiter != nil
 }
