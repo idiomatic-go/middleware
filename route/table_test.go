0afe96dc6e79bce1477c918fd417e65e449dbb18
idiomatic-go <markbwilson@outlook.com>
2022-12-10 10:48:27 -0600

    Update table_test.go

--- route/table_test.go
+++ route/table_test.go
@@ -8,45 +8,46 @@ import (
 func ExampleTable_SetDefault() {
 	t := newTable()
 
-	fmt.Printf("result : [empty:%v]\n", t.isEmpty())
+	fmt.Printf("result : empty() -> [%v]\n", t.isEmpty())
 
 	r := t.Lookup(nil)
-	fmt.Printf("result : [route:%v] [default:%v]\n", r.(*route).default_, r.(*route).name == DefaultName)
+	fmt.Printf("result : lookup(nil) -> [default:%v]\n", r.(*route).name == DefaultName)
 	//fmt.Printf("IsDefault : %v\n", r.(*route).name == DefaultName)
 
 	t.SetDefault(newRoute("not-default"))
 	r = t.Lookup(nil)
-	fmt.Printf("result : [default:%v]\n", r.(*route).name == DefaultName)
+	fmt.Printf("result : lookup(req) -> [default:%v]\n", r.(*route).name == DefaultName)
 
 	//Output:
-	//result : [empty:true]
-	//result : [route:{-1 -1 -1}] [default:true]
-	//result : [default:false]
+	//result : empty() -> [true]
+	//result : lookup(nil) -> [default:true]
+	//result : lookup(req) -> [default:false]
+
 }
 
 func ExampleTable_Interact() {
 	name := "test-route"
 	t := newTable()
-	fmt.Printf("result : [empty:%v]\n", t.isEmpty())
+	fmt.Printf("result : empty() -> [%v]\n", t.isEmpty())
 
 	ok := t.Add(nil)
-	fmt.Printf("result : [add(nil):%v] [count:%v] [exists:%v] [lookup:%v]\n", ok, t.count(), t.Exists(name), t.LookupByName(name))
+	fmt.Printf("result : add(nil) -> [%v] [count:%v] [exists:%v] [lookup:%v]\n", ok, t.count(), t.Exists(name), t.LookupByName(name))
 
 	ok = t.Add(newRoute(name))
-	fmt.Printf("result : [add(route):%v] [count:%v] [exists:%v] [lookup:%v]\n", ok, t.count(), t.Exists(name), t.LookupByName(name))
+	fmt.Printf("result : add(route) -> [%v] [count:%v] [exists:%v] [lookup:%v]\n", ok, t.count(), t.Exists(name), t.LookupByName(name))
 
 	ok = t.Remove("")
-	fmt.Printf("result : [remove(\"\"):%v] [count:%v] [exists:%v] [lookup:%v]\n", ok, t.count(), t.Exists(name), t.LookupByName(name))
+	fmt.Printf("result : remove(\"\") -> [%v] [count:%v] [exists:%v] [lookup:%v]\n", ok, t.count(), t.Exists(name), t.LookupByName(name))
 
 	ok = t.Remove(name)
-	fmt.Printf("result : [remove(name):%v] [count:%v] [exists:%v] [lookup:%v]\n", ok, t.count(), t.Exists(name), t.LookupByName(name))
+	fmt.Printf("result : remove(name) -> [%v] [count:%v] [exists:%v] [lookup:%v]\n", ok, t.count(), t.Exists(name), t.LookupByName(name))
 
 	//Output:
-	//result : [empty:true]
-	//result : [add(nil):false] [count:0] [exists:false] [lookup:<nil>]
-	//result : [add(route):true] [count:1] [exists:true] [lookup:&{test-route {-1 -1 -1} {-1 -1 -1} false false <nil>}]
-	//result : [remove(""):false] [count:1] [exists:true] [lookup:&{test-route {-1 -1 -1} {-1 -1 -1} false false <nil>}]
-	//result : [remove(name):true] [count:0] [exists:false] [lookup:<nil>]
+	//result : empty() -> [true]
+	//result : add(nil) -> [false] [count:0] [exists:false] [lookup:<nil>]
+	//result : add(route) -> [true] [count:1] [exists:true] [lookup:&{test-route {-1 -1 -1} {-1 -1 -1} false false <nil>}]
+	//result : remove("") -> [false] [count:1] [exists:true] [lookup:&{test-route {-1 -1 -1} {-1 -1 -1} false false <nil>}]
+	//result : remove(name) -> [true] [count:0] [exists:false] [lookup:<nil>]
 
 }
 
