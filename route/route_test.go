bfea33983b2400844448574b4ff90473148aa081
idiomatic-go <markbwilson@outlook.com>
2022-12-11 11:27:42 -0600

    Testing

--- route/route_test.go
+++ route/route_test.go
@@ -8,50 +8,50 @@ import (
 
 func Example_NewRouteWithConfig_Error() {
 	_, err := newRouteWithConfig("", NilValue, 100, 10, true, false)
-	fmt.Printf("result : (\"\",_,_) -> [err:%v]\n", err)
+	fmt.Printf("test: new(\"\",_,_) -> [err:%v]\n", err)
 
 	_, err = newRouteWithConfig("test", NilValue, 100, 0, true, false)
-	fmt.Printf("result : (_,100,0) -> [err:%v]\n", err)
+	fmt.Printf("test: new(_,100,0) -> [err:%v]\n", err)
 
 	_, err = newRouteWithConfig("test", NilValue, 100, NilValue, true, false)
-	fmt.Printf("result : (_,100,NilValue) -> [err:%v]\n", err)
+	fmt.Printf("test: new(_,100,NilValue) -> [err:%v]\n", err)
 
 	_, err = newRouteWithConfig("test", NilValue, 0, 10, true, false)
-	fmt.Printf("result : (_,0,10) -> [err:%v]\n", err)
+	fmt.Printf("test: new(_,0,10) -> [err:%v]\n", err)
 
 	_, err = newRouteWithConfig("test", NilValue, NilValue, 10, true, false)
-	fmt.Printf("result : (_,NilValue,NilValue) -> [err:%v]\n", err)
+	fmt.Printf("test: new(_,NilValue,NilValue) -> [err:%v]\n", err)
 
 	//Output:
-	//result : ("",_,_) -> [err:invalid argument : route name is empty]
-	//result : (_,100,0) -> [err:invalid argument : limit is configured but burst is not]
-	//result : (_,100,NilValue) -> [err:invalid argument : limit is configured but burst is not]
-	//result : (_,0,10) -> [err:invalid argument : burst is configured but limit is not]
-	//result : (_,NilValue,NilValue) -> [err:invalid argument : burst is configured but limit is not]
+	//test: new("",_,_) -> [err:invalid argument : route name is empty]
+	//test: new(_,100,0) -> [err:invalid argument : limit is configured but burst is not]
+	//test: new(_,100,NilValue) -> [err:invalid argument : limit is configured but burst is not]
+	//test: new(_,0,10) -> [err:invalid argument : burst is configured but limit is not]
+	//test: new(_,NilValue,NilValue) -> [err:invalid argument : burst is configured but limit is not]
 }
 
 func Example_NewRouteWithConfig() {
 	r, err := newRouteWithConfig("test", NilValue, 0, 0, true, false)
-	fmt.Printf("result : (0,0) -> [config:%v] [err:%v] [limiter:%v] [duration:%v] [allow:%v]\n", r.default_, err, r.IsRateLimiter(), r.Duration(), r.Allow())
+	fmt.Printf("test: new(0,0) -> [config:%v] [err:%v] [limiter:%v] [duration:%v] [allow:%v]\n", r.default_, err, r.IsRateLimiter(), r.Duration(), r.Allow())
 
 	r, err = newRouteWithConfig("test", NilValue, 100, 10, true, false)
-	fmt.Printf("result : (100,0) -> [config:%v] [err:%v] [limiter:%v] [allow:%v]\n", r.default_, err, r.IsRateLimiter(), r.Allow())
+	fmt.Printf("test: new(100,0) -> [config:%v] [err:%v] [limiter:%v] [allow:%v]\n", r.default_, err, r.IsRateLimiter(), r.Allow())
 
 	r, err = newRouteWithConfig("test", NilValue, rate.Inf, NilValue, true, false)
-	fmt.Printf("result : (Inf,NilValue) -> [config:%v] [err:%v] [limiter:%v] [allow:%v]\n", r.default_, err, r.IsRateLimiter(), r.Allow())
+	fmt.Printf("test: new(Inf,NilValue) -> [config:%v] [err:%v] [limiter:%v] [allow:%v]\n", r.default_, err, r.IsRateLimiter(), r.Allow())
 
 	r, err = newRouteWithConfig("test", NilValue, rate.Inf, 0, true, false)
-	fmt.Printf("result : (Inf,0) -> [config:%v] [err:%v] [limiter:%v] [allow:%v]\n", r.default_, err, r.IsRateLimiter(), r.Allow())
+	fmt.Printf("test: new(Inf,0) -> [config:%v] [err:%v] [limiter:%v] [allow:%v]\n", r.default_, err, r.IsRateLimiter(), r.Allow())
 
 	r, err = newRouteWithConfig("test", NilValue, rate.Inf, 1234, true, false)
-	fmt.Printf("result : (Inf,1234) -> [config:%v] [err:%v] [limiter:%v] [allow:%v]\n", r.default_, err, r.IsRateLimiter(), r.Allow())
+	fmt.Printf("test: new(Inf,1234) -> [config:%v] [err:%v] [limiter:%v] [allow:%v]\n", r.default_, err, r.IsRateLimiter(), r.Allow())
 
 	//Output:
-	//result : (0,0) -> [config:{-1 -1 -1}] [err:<nil>] [limiter:false] [duration:0s] [allow:true]
-	//result : (100,0) -> [config:{-1 100 10}] [err:<nil>] [limiter:true] [allow:true]
-	//result : (Inf,NilValue) -> [config:{-1 1.7976931348623157e+308 1}] [err:<nil>] [limiter:true] [allow:true]
-	//result : (Inf,0) -> [config:{-1 1.7976931348623157e+308 1}] [err:<nil>] [limiter:true] [allow:true]
-	//result : (Inf,1234) -> [config:{-1 1.7976931348623157e+308 1234}] [err:<nil>] [limiter:true] [allow:true]
+	//test: new(0,0) -> [config:{-1 -1 -1}] [err:<nil>] [limiter:false] [duration:0s] [allow:true]
+	//test: new(100,0) -> [config:{-1 100 10}] [err:<nil>] [limiter:true] [allow:true]
+	//test: new(Inf,NilValue) -> [config:{-1 1.7976931348623157e+308 1}] [err:<nil>] [limiter:true] [allow:true]
+	//test: new(Inf,0) -> [config:{-1 1.7976931348623157e+308 1}] [err:<nil>] [limiter:true] [allow:true]
+	//test: new(Inf,1234) -> [config:{-1 1.7976931348623157e+308 1234}] [err:<nil>] [limiter:true] [allow:true]
 
 }
 
@@ -71,7 +71,7 @@ func Example_RateLimiter_DisallowAll() {
 
 func Example_RateLimiter_AllowAll() {
 	r, err := newRouteWithConfig("test", NilValue, rate.Inf, 0, false, false)
-	fmt.Printf("result : (Inf,0) -> [config:%v] [err:%v] [limiter:%v] [allow:%v]\n", r.default_, err, r.IsRateLimiter(), r.Allow())
+	fmt.Printf("test: new(Inf,0) -> [config:%v] [err:%v] [limiter:%v] [allow:%v]\n", r.default_, err, r.IsRateLimiter(), r.Allow())
 
 	i := 0
 	for ; i < 100; i++ {
@@ -81,13 +81,13 @@ func Example_RateLimiter_AllowAll() {
 	}
 
 	//Output:
-	//result : (Inf,0) -> [config:{-1 1.7976931348623157e+308 1}] [err:<nil>] [limiter:true] [allow:true]
+	//test: new(Inf,0) -> [config:{-1 1.7976931348623157e+308 1}] [err:<nil>] [limiter:true] [allow:true]
 
 }
 
 func Example_RateLimiter_AllowLimit() {
 	r, err := newRouteWithConfig("test", NilValue, 1, 1, false, false)
-	fmt.Printf("result : (1,1) -> [config:%v] [err:%v] [limiter:%v] [allow:%v]\n", r.default_, err, r.IsRateLimiter(), r.Allow())
+	fmt.Printf("test: new(1,1) -> [config:%v] [err:%v] [limiter:%v] [allow:%v]\n", r.default_, err, r.IsRateLimiter(), r.Allow())
 
 	allow := 0
 	disallow := 0
@@ -102,9 +102,9 @@ func Example_RateLimiter_AllowLimit() {
 			time.Sleep(time.Second)
 		}
 	}
-	fmt.Printf("result : [count:10] [allow:%v] [disallow:%v]\n", allow, disallow)
+	fmt.Printf("test: () [count:10] [allow:%v] [disallow:%v]\n", allow, disallow)
 	//Output:
-	//result : (1,1) -> [config:{-1 1 1}] [err:<nil>] [limiter:true] [allow:true]
-	//result : [count:10] [allow:3] [disallow:7]
+	//test: new(1,1) -> [config:{-1 1 1}] [err:<nil>] [limiter:true] [allow:true]
+	//test: () [count:10] [allow:3] [disallow:7]
 
 }
--- route/table_test.go
+++ route/table_test.go
@@ -8,76 +8,76 @@ import (
 func ExampleTable_SetDefault() {
 	t := newTable()
 
-	fmt.Printf("result : empty() -> [%v]\n", t.isEmpty())
+	fmt.Printf("test: Empty() -> [%v]\n", t.isEmpty())
 
 	r := t.Lookup(nil)
-	fmt.Printf("result : lookup(nil) -> [default:%v]\n", r.(*route).name == DefaultName)
+	fmt.Printf("test: Lookup(nil) -> [default:%v]\n", r.(*route).name == DefaultName)
 	//fmt.Printf("IsDefault : %v\n", r.(*route).name == DefaultName)
 
 	t.SetDefault(newRoute("not-default"))
 	r = t.Lookup(nil)
-	fmt.Printf("result : lookup(req) -> [default:%v]\n", r.(*route).name == DefaultName)
+	fmt.Printf("test: Lookup(req) -> [default:%v]\n", r.(*route).name == DefaultName)
 
 	//Output:
-	//result : empty() -> [true]
-	//result : lookup(nil) -> [default:true]
-	//result : lookup(req) -> [default:false]
+	//test: Empty() -> [true]
+	//test: Lookup(nil) -> [default:true]
+	//test: Lookup(req) -> [default:false]
 
 }
 
 func ExampleTable_Interact() {
 	name := "test-route"
 	t := newTable()
-	fmt.Printf("result : empty() -> [%v]\n", t.isEmpty())
+	fmt.Printf("test: Empty() -> [%v]\n", t.isEmpty())
 
 	ok := t.Add(nil)
-	fmt.Printf("result : add(nil) -> [%v] [count:%v] [exists:%v] [lookup:%v]\n", ok, t.count(), t.Exists(name), t.LookupByName(name))
+	fmt.Printf("test: Add(nil) -> [ok:%v] [count:%v] [exists:%v] [lookup:%v]\n", ok, t.count(), t.Exists(name), t.LookupByName(name))
 
 	ok = t.Add(newRoute(name))
-	fmt.Printf("result : add(route) -> [%v] [count:%v] [exists:%v] [lookup:%v]\n", ok, t.count(), t.Exists(name), t.LookupByName(name))
+	fmt.Printf("test: Add(route) -> [ok:%v] [count:%v] [exists:%v] [lookup:%v]\n", ok, t.count(), t.Exists(name), t.LookupByName(name))
 
 	t.Remove("")
-	fmt.Printf("result : remove(\"\") -> [count:%v] [exists:%v] [lookup:%v]\n", t.count(), t.Exists(name), t.LookupByName(name))
+	fmt.Printf("test: Remove(\"\") -> [count:%v] [exists:%v] [lookup:%v]\n", t.count(), t.Exists(name), t.LookupByName(name))
 
 	t.Remove(name)
-	fmt.Printf("result : remove(name) -> [count:%v] [exists:%v] [lookup:%v]\n", t.count(), t.Exists(name), t.LookupByName(name))
+	fmt.Printf("test: Remove(name) -> [count:%v] [exists:%v] [lookup:%v]\n", t.count(), t.Exists(name), t.LookupByName(name))
 
 	//Output:
-	//result : empty() -> [true]
-	//result : add(nil) -> [false] [count:0] [exists:false] [lookup:<nil>]
-	//result : add(route) -> [true] [count:1] [exists:true] [lookup:&{test-route {-1 -1 -1} {-1 -1 -1} false false <nil>}]
-	//result : remove("") -> [count:1] [exists:true] [lookup:&{test-route {-1 -1 -1} {-1 -1 -1} false false <nil>}]
-	//result : remove(name) -> [count:0] [exists:false] [lookup:<nil>]
+	//test: Empty() -> [true]
+	//test: Add(nil) -> [ok:false] [count:0] [exists:false] [lookup:<nil>]
+	//test: Add(route) -> [ok:true] [count:1] [exists:true] [lookup:&{test-route {-1 -1 -1} {-1 -1 -1} false false <nil>}]
+	//test: Remove("") -> [count:1] [exists:true] [lookup:&{test-route {-1 -1 -1} {-1 -1 -1} false false <nil>}]
+	//test: Remove(name) -> [count:0] [exists:false] [lookup:<nil>]
 
 }
 
 func ExampleTable_Lookup() {
 	name := "test-route"
 	t := newTable()
-	fmt.Printf("result : [empty:%v]\n", t.isEmpty())
+	fmt.Printf("test: Empty() -> [%v]\n", t.isEmpty())
 
 	r := t.Lookup(nil)
-	fmt.Printf("result : [lookup(nil):%v]\n", r)
+	fmt.Printf("test: Lookup(nil) -> [route:%v]\n", r)
 
 	req, _ := http.NewRequest("", "http://localhost:8080/accesslog", nil)
 	r = t.Lookup(req)
-	fmt.Printf("result : [lookup(req):%v]\n", r)
+	fmt.Printf("test: Lookup(req) -> [route:%v]\n", r)
 
 	ok := t.Add(newRoute(name))
-	fmt.Printf("result : [add(route):%v] [count:%v] [exists:%v]\n", ok, t.count(), t.Exists(name))
+	fmt.Printf("test: Add(route) -> [route:%v] [count:%v] [exists:%v]\n", ok, t.count(), t.Exists(name))
 
 	t.SetMatcher(func(req *http.Request) string {
 		return name
 	},
 	)
 	r = t.Lookup(req)
-	fmt.Printf("result : [lookup(req):%v]\n", r)
+	fmt.Printf("test: Lookup(req) ->  [route:%v]\n", r)
 
 	//Output:
-	//result : [empty:true]
-	//result : [lookup(nil):&{* {-1 -1 -1} {-1 -1 -1} false false <nil>}]
-	//result : [lookup(req):&{* {-1 -1 -1} {-1 -1 -1} false false <nil>}]
-	//result : [add(route):true] [count:1] [exists:true]
-	//result : [lookup(req):&{test-route {-1 -1 -1} {-1 -1 -1} false false <nil>}]
+	//test: Empty() -> [true]
+	//test: Lookup(nil) -> [route:&{* {-1 -1 -1} {-1 -1 -1} false false <nil>}]
+	//test: Lookup(req) -> [route:&{* {-1 -1 -1} {-1 -1 -1} false false <nil>}]
+	//test: Add(route) -> [route:true] [count:1] [exists:true]
+	//test: Lookup(req) ->  [route:&{test-route {-1 -1 -1} {-1 -1 -1} false false <nil>}]
 
 }
