374991b737fdf1097f878f089459e00aba55fb0a
idiomatic-go <markbwilson@outlook.com>
2022-12-19 08:19:46 -0600

    Adding tests

--- automation/timeout_test.go
+++ automation/timeout_test.go
@@ -1,3 +1,22 @@
 package automation
 
+import "fmt"
+
 // TODO : test nil attribute
+
+func Example_newTimeout() {
+	t := newTimeout("test-route", nil, newTable())
+	fmt.Printf("test: newTimeout() -> [enabled:%v] [name:%v] [default:%v] [current:%v]\n", t.enabled, t.name, t.defaultC, t.current)
+
+	t = newTimeout("test-route2", NewTimeoutConfig(2000, 504), newTable())
+	fmt.Printf("test: newTimeout() -> [enabled:%v] [name:%v] [default:%v] [current:%v]\n", t.enabled, t.name, t.defaultC, t.current)
+
+	t2 := cloneTimeout(t)
+	t2.enabled = false
+	fmt.Printf("test: cloneTimeout() -> [prev-enabled:%v] [prev-name:%v] [curr-enabled:%v] [curr-name:%v]\n", t.enabled, t.name, t2.enabled, t2.name)
+
+	//Output:
+	//test: newTimeout() -> [enabled:false] [name:test-route] [default:{-1 -1}] [current:{-1 -1}]
+	//test: newTimeout() -> [enabled:true] [name:test-route2] [default:{2000 504}] [current:{2000 504}]
+	//test: cloneTimeout() -> [prev-enabled:true] [prev-name:test-route2] [curr-enabled:false] [curr-name:test-route2]
+}
