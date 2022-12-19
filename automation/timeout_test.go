181531ffd12eaa3ea547dbd41da50f9831981836
idiomatic-go <markbwilson@outlook.com>
2022-12-19 08:33:14 -0600

    Update timeout_test.go

--- automation/timeout_test.go
+++ automation/timeout_test.go
@@ -20,3 +20,29 @@ func Example_newTimeout() {
 	//test: newTimeout() -> [enabled:true] [name:test-route2] [default:{2000 504}] [current:{2000 504}]
 	//test: cloneTimeout() -> [prev-enabled:true] [prev-name:test-route2] [curr-enabled:false] [curr-name:test-route2]
 }
+
+func Example_Controller_ReadOnly() {
+	t := newTimeout("test-route", NewTimeoutConfig(2000, 504), newTable())
+	fmt.Printf("test: IsEnabled() -> [%v]\n", t.IsEnabled())
+
+	fmt.Printf("test: Duration() -> [%v]\n", t.Duration())
+
+	fmt.Printf("test: StatusCode(200) -> [%v]\n", t.StatusCode(200))
+
+	t = newTimeout("test-route", NewTimeoutConfig(2000, NilValue), newTable())
+	fmt.Printf("test: StatusCode(503) -> [%v]\n", t.StatusCode(503))
+
+	a := t.Attribute("")
+	fmt.Printf("test: Attribute(\"\") -> [name:%v] [value:%v] [string:%v]\n", a.Name(), a.Value(), a)
+
+	a = t.Attribute(TimeoutName)
+	fmt.Printf("test: Attribute(\"Timeout\") -> [name:%v] [value:%v] [string:%v]\n", a.Name(), a.Value(), a)
+
+	//Output:
+	//test: IsEnabled() -> [true]
+	//test: Duration() -> [2s]
+	//test: StatusCode(200) -> [504]
+	//test: StatusCode(503) -> [503]
+	//test: Attribute("") -> [name:] [value:<nil>] [string:nil]
+	//test: Attribute("Timeout") -> [name:timeout] [value:2000] [string:2000]
+}
