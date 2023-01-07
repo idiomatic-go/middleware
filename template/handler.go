bfae36b490833499c377c85fcadc038b108a0e31
idiomatic-go <markbwilson@outlook.com>
2023-01-07 07:23:26 -0600

    Refactoring

--- template/handler.go
+++ template/handler.go
@@ -7,45 +7,64 @@ import (
 
 type ErrorHandler interface {
 	Handle(location string, errs ...error) *Status
+	HandleStatus(s *Status) *Status
 }
 
-/*
-type ErrorHandlerFn func(location string, errs ...error) *Status
-
-func (ErrorHandlerFn)Handle(string,...error) *Status {
-    return nil
-}
-
-
-*/
 type NoOpHandler struct{}
 
 func (NoOpHandler) Handle(location string, errs ...error) *Status {
+	if len(errs) == 0 || (len(errs) == 1 && errs[0] == nil) {
+		return NewStatusOk()
+	}
 	return NewStatusError(location, errs...)
+}
+
+func (NoOpHandler) HandleStatus(s *Status) *Status {
+	return s
 }
 
 type DebugHandler struct{}
 
-func (DebugHandler) Handle(location string, errs ...error) *Status {
+func (h DebugHandler) Handle(location string, errs ...error) *Status {
 	if len(errs) == 0 || (len(errs) == 1 && errs[0] == nil) {
-	} else {
-		if location == "" {
-			location = "[]"
-		}
-		fmt.Printf("[%v %v]\n", location, errs)
+		return NewStatusOk()
 	}
-	return NewStatus(StatusInternal, location, errs...)
+	return h.HandleStatus(NewStatus(StatusInternal, location, errs...))
+}
+
+func (h DebugHandler) HandleStatus(s *Status) *Status {
+	if s != nil && s.IsErrors() {
+		if len(s.errs) == 0 || (len(s.errs) == 1 && s.errs[0] == nil) {
+		} else {
+			if s.location == "" {
+				s.location = "[]"
+			}
+			fmt.Printf("[%v %v]\n", s.location, s.errs)
+			s.errs = nil
+		}
+	}
+	return s
 }
 
 type LogHandler struct{}
 
-func (LogHandler) Handle(location string, errs ...error) *Status {
+func (h LogHandler) Handle(location string, errs ...error) *Status {
 	if len(errs) == 0 || (len(errs) == 1 && errs[0] == nil) {
-	} else {
-		if location == "" {
-			location = "[]"
-		}
-		log.Println(location, errs)
+		return NewStatusOk()
 	}
-	return NewStatus(StatusInternal, location, errs...)
+	return h.HandleStatus(NewStatus(StatusInternal, location, errs...))
+}
+
+func (h LogHandler) HandleStatus(s *Status) *Status {
+	if s != nil && s.IsErrors() {
+		if len(s.errs) == 0 || (len(s.errs) == 1 && s.errs[0] == nil) {
+		} else {
+			if s.location == "" {
+				s.location = "[]"
+			}
+			log.Println(s.location, s.errs)
+			s.errs = nil
+		}
+	}
+	return s
 }
--- template/handler_test.go
+++ template/handler_test.go
@@ -5,8 +5,8 @@ import "fmt"
 func ExampleDebugHandler_Handle() {
 	var h DebugHandler
 
-	fmt.Printf("test: Handle() -> %v\n", h.Handle("location", nil))
+	fmt.Printf("test: Handle() -> [%v]\n", h.Handle("location", nil))
 
 	//Output:
-	//test: Handle() -> 0 The operation was successful []
+	//test: Handle() -> [0 The operation was successful]
 }
