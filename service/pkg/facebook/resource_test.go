0311138b5fabeff189ee30331d97ed5f39e1d735
idiomatic-go <markbwilson@outlook.com>
2023-01-11 08:05:51 -0600

    Adding resources

---
+++ service/pkg/facebook/resource_test.go
@@ -0,0 +1,30 @@
+package google
+
+import (
+	"fmt"
+	"github.com/idiomatic-go/middleware/actuator"
+	"github.com/idiomatic-go/middleware/host"
+	"github.com/idiomatic-go/middleware/template"
+	"net/http"
+	"time"
+)
+
+func init() {
+	name := "facebook:home"
+	host.WrapDefaultTransport()
+	actuator.EgressTable.SetDefaultActuator(actuator.DefaultActuatorName, nil, actuator.NewTimeoutConfig(time.Second*4, http.StatusGatewayTimeout))
+	actuator.EgressTable.Add(name, nil)
+	actuator.EgressTable.SetMatcher(func(req *http.Request) string {
+		return name
+	},
+	)
+}
+
+func ExampleSearch_Success() {
+	buff, status := Search[template.DebugHandler](nil)
+	fmt.Printf("test: Search() -> [%v] [content:%v]\n", status, buff != nil)
+
+	//Output:
+	//test: Search() -> [0 Successful] [content:true]
+
+}
---
+++ service/pkg/twitter/resource.go
@@ -0,0 +1,36 @@
+package google
+
+import (
+	"github.com/idiomatic-go/middleware/template"
+	"net/http"
+	"reflect"
+)
+
+const (
+	uri = "https://www.twitter.com"
+)
+
+type pkg struct{}
+
+var pkgPath = reflect.TypeOf(any(pkg{})).PkgPath()
+var homeLoc = pkgPath + "/home"
+
+func Search[E template.ErrorHandler](req *http.Request) ([]byte, *template.Status) {
+	var e E
+
+	if req != nil && template.IsContextContent(req.Context()) {
+		return template.ProcessContextContent[[]byte, E](req.Context())
+	}
+	newReq, err := http.NewRequest("GET", uri, nil)
+	if err != nil {
+		return nil, e.Handle(homeLoc, err)
+	}
+	resp, status := template.Do(newReq)
+	if status.IsErrors() {
+		return nil, e.HandleStatus(status)
+	}
+	if !status.Ok() {
+		return nil, status
+	}
+	return template.Unmarshal[[]byte, E](resp)
+}
